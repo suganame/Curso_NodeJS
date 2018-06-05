6ff33876f33bea80301a37769a51a4900f718b5b
suganame <guilherme_suganame@yahoo.com.br>
2018-06-05 00:54:04 -0300

    Cookie e filters

--- menu/app/public/admin/js/includes/navbar.js
+++ menu/app/public/admin/js/includes/navbar.js
@@ -1,11 +1,21 @@
-$(document).ready(function() {
+$(document).ready(function() {	
 	var containerLeft = $('.container-left');
 	var containerRight = $('.container-right');
 	var submenu = $('.submenu-item a');	
 	var menuToggle = $('.container-toggle');
 	var menuItem = $('.menu-item');
 
-	// navBar.html('');
+	if($.cookie('navbar-collapse') == 1){
+		menuItem.hide();	
+		$('.menu-icon-after').hide();
+		$('.menu-icon-before').addClass('fa-rotate-180');
+		containerLeft.addClass('container-left-toggled');
+		containerRight.addClass('container-right-max');
+	}else{
+		console.log('teste');
+		containerLeft.removeClass('container-left-toggled');
+		containerRight.removeClass('container-right-max');
+	}
 
 	submenu.click(function(event) {
 
@@ -18,13 +28,6 @@ $(document).ready(function() {
 			$(this).find('.menu-icon-after').addClass('fa-rotate-270');
 
 		$(this).parent('li').find('.submenu').slideToggle(300);
-
-		// if($(this).parent('li').find('.submenu').hasClass('submenu-active')){
-		// 	$(this).parent('li').find('.submenu').removeClass('submenu-active');
-		// }
-		// else{
-		// 	$(this).parent('li').find('.submenu').addClass('submenu-active');	
-		// }
 	});
 
 	menuToggle.click(function(event) {
@@ -40,8 +43,9 @@ $(document).ready(function() {
 				menuItem.show();	
 				$('.menu-icon-after').show();
 			}, 150);
-			
-			
+		
+			$.cookie('navbar-collapse', 0);
+			console.log($.cookie('navbar-collapse'));
 		}
 		else{
 			menuItem.hide();	
@@ -49,9 +53,11 @@ $(document).ready(function() {
 			$('.menu-icon-before').addClass('fa-rotate-180');
 			containerLeft.addClass('container-left-toggled');
 			containerRight.addClass('container-right-max');
-			
+
+			$.cookie('navbar-collapse', 1);
+
+			console.log($.cookie('navbar-collapse'));
 		}
-		// });
 
 	});
 });
