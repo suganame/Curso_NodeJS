be31e706252baa476e632ca2602f321a5f805e70
Guilherme <guilherme_suganame@yahoo.com.br>
2018-06-08 09:03:32 -0300

    Alterações para subir no site

--- menu/app.js
+++ menu/app.js
@@ -1,5 +1,7 @@
-var
-	app = require('./config/serverConfig');
+var app = require('./config/serverConfig');
+// var port = process.env.PORT || 3000;
+
 
 app.listen(3000, function(){
+	console.log('Servidor online');
 });
